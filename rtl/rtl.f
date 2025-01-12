b412fd4ecfc58aa38efa552ef6939b1c38d7119b
Sean Li <theseanisalie@gmail.com>
2025-01-11 21:48:15 -0800

    Updated rtl

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -1,6 +1,4 @@
--I third_party/alexforencich_uart/rtl/uart_rx.v/
-third_party/alexforencich_uart/rtl/uart_tx.v/
+-I third_party/alexforencich_uart/rtl/uart_rx.v
+third_party/alexforencich_uart/rtl/uart_tx.v
 
-rtl/top.sv
-rtl/pll.sv
 rtl/uart_comm.sv
--- rtl/uart_comm.sv
+++ rtl/uart_comm.sv
@@ -23,13 +23,13 @@ module uart_comm
 wire xd_w;
 
 
-uart_tx uart_tx_inst(.clk, .rst, .s_axis_tdata(data_i), .s_axis_tvalid(valid_i),
+uart_tx uart_tx(.clk, .rst, .s_axis_tdata(data_i), .s_axis_tvalid(valid_i),
 .s_axis_tready(), .txd(xd_w), .busy(busy_tx_o), .prescale);
 
 
 //wire busy_rx_w, valid_rx_w;
 
-uart_rx uart_rx_inst(.clk, .rst, .m_axis_tdata(data_o), .m_axis_tvalid(), 
+uart_rx uart_rx(.clk, .rst, .m_axis_tdata(data_o), .m_axis_tvalid(), 
 .m_axis_tready(ready_i), .rxd(xd_w), .busy(busy_rx_o), .overrun_error(), .frame_error(), 
 .prescale); // Do I even need to sink the outputs
 
