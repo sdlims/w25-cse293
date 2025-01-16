2dd5713192ddf9d5a4fdedefa20eea7cb5cf9823
Sean Li <theseanisalie@gmail.com>
2025-01-15 21:32:06 -0800

    Updated rtl with tx/rx interface

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -1,4 +1,6 @@
--I third_party/alexforencich_uart/rtl/uart_rx.v
-third_party/alexforencich_uart/rtl/uart_tx.v
-
+-Ithird_party/alexforencich_uart/rtl/
+third_party/alexforencich_uart/rtl/uart_rx.v
+third_party/alexforencich_uart/rtl/uart_rx.v
 rtl/uart_comm.sv
+
+
--- rtl/uart_comm.sv
+++ rtl/uart_comm.sv
@@ -8,32 +8,28 @@ module uart_comm
     input   wire                        clk,
     input   wire                        rst,
 
-    input   wire                        ready_i,
-    input   wire                        valid_i,
-    
-    input   wire [15:0]                 prescale,
-    input   wire [DATA_WIDTH-1:0]       data_i,
-    output  wire                        busy_tx_o,
-    output  wire                        busy_rx_o,
-    output  wire [DATA_WIDTH-1:0]       data_o
+    input   wire                        rx_i,
+    output  wire                        tx_o
 );
+
+logic [7:0] data;
 
 /* verilator lint_off WIDTHEXPAND */
 
-wire xd_w;
+logic tx_ready;
 
 
-uart_tx uart_tx(.clk, .rst, .s_axis_tdata(data_i), .s_axis_tvalid(valid_i),
-.s_axis_tready(), .txd(xd_w), .busy(busy_tx_o), .prescale);
+uart_tx uart_tx(.clk, .rst, .s_axis_tdata(data), .s_axis_tvalid(rx_valid),
+.s_axis_tready(tx_ready), .txd(tx_o), .busy(), .prescale(1));
 
 
 //wire busy_rx_w, valid_rx_w;
+logic rx_valid;
 
-uart_rx uart_rx(.clk, .rst, .m_axis_tdata(data_o), .m_axis_tvalid(), 
-.m_axis_tready(ready_i), .rxd(xd_w), .busy(busy_rx_o), .overrun_error(), .frame_error(), 
-.prescale); // Do I even need to sink the outputs
+uart_rx uart_rx(.clk, .rst, .m_axis_tdata(data), .m_axis_tvalid(rx_valid), 
+.m_axis_tready(tx_ready), .rxd(rx_i), .busy(), .overrun_error(), .frame_error(), 
+.prescale(1));
 
 /* verilator lint_off WIDTHEXPAND */
-// assign data_o = data_w;
 
 endmodule
