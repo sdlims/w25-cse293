8e9850e5199b3169f1805385b9526821e97a8194
Sean Li <theseanisalie@gmail.com>
2025-03-28 13:53:29 -0700

    Updated rtl for divide

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -1,6 +1,19 @@
--Ithird_party/alexforencich_uart/rtl/
-third_party/alexforencich_uart/rtl/uart_rx.v
-third_party/alexforencich_uart/rtl/uart_rx.v
+
+-I${BASEJUMP_STL_DIR}/bsg_misc
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_counter_up_down.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_imul_iterative.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_idiv_iterative.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_mux_one_hot.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_idiv_iterative_controller.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_adder_cin.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_dff_en.sv
+${BASEJUMP_STL_DIR}/bsg_misc/bsg_counter_clear_up.sv
+
+-I${ALEXFORENCICH_UART_DIR}/rtl
+${ALEXFORENCICH_UART_DIR}/rtl/uart_rx.v
+${ALEXFORENCICH_UART_DIR}/rtl/uart_tx.v
+${ALEXFORENCICH_UART_DIR}/rtl/uart.v
+
 rtl/uart_comm.sv
 
 
--- rtl/uart_comm.sv
+++ rtl/uart_comm.sv
@@ -26,23 +26,36 @@ uart_rx uart_rx(.clk(clk), .rst(rst), .m_axis_tdata(rx_data), .m_axis_tvalid(rx_
 uart_tx uart_tx(.clk(clk), .rst(rst), .s_axis_tdata(tx_data), .s_axis_tvalid(tx_tvalid),
 .s_axis_tready(tx_tready), .txd(tx_o), .busy(), .prescale(35));
 
+logic [0:0] idiv, div_valid;
+logic [63:0] div_o;
+bsg_idiv_iterative #(.width_p(32), .bitstack_p(0), .bits_per_iter_p(1))
+idiv_inst (.clk_i(clk), .reset_i(rst), .v_i(idiv), .ready_and_o(), .dividend_i(operand_1_q), .divisor_i(operand_2_q), .signed_div_i(1'b1),
+.v_o(div_valid), .quotient_o(div_o), .remainder_o(), .yumi_i(1'b1));
+
 logic [7:0] opcode_d, opcode_q;
 logic [15:0] length_d, length_q;
 logic [15:0] frame_cnt_d, frame_cnt_q;
 logic [31:0] operand_1_d, operand_1_q;
 logic [31:0] operand_2_d, operand_2_q;
 
+logic [0:0] cnt_d, cnt_q;
 always_ff @( posedge clk ) begin : ps_ns
     if (rst) begin
         state_q <= 4'd0;
         frame_cnt_q <= 16'd0;
         opcode_q <= 8'h00;
         length_q <= '0;
+        operand_1_q <= 32'd0;
+        operand_2_q <= 32'd0;
+        cnt_q <= 1'b0;
     end else begin
         state_q <= state_d;
         frame_cnt_q <= frame_cnt_d;
         opcode_q <= opcode_d;
         length_q <= length_d;
+        operand_1_q <= operand_1_d;
+        operand_2_q <= operand_2_d;
+        cnt_q <= cnt_d;
     end
 end
 
@@ -51,10 +64,11 @@ always_comb begin : state_machine
     opcode_d = opcode_q;
     rx_tready = 1'b0;
     tx_tvalid = 1'b0;
+    idiv = 1'b0;
     tx_data = 'x;
     length_d = length_q;
     frame_cnt_d = frame_cnt_q;
-
+    cnt_d = cnt_q;
     case(state_q)
         4'd0: begin // OPCODE
             rx_tready = 1'b1;
@@ -118,16 +132,130 @@ always_comb begin : state_machine
                 tx_data = rx_data;
                 if (frame_cnt_d == length_q) begin
                     state_d = 4'd0;
+                    frame_cnt_d = '0;
                 end else begin
                     state_d = 4'd4;
+                    frame_cnt_d = frame_cnt_q;
                 end
             end
         end
 
         4'd5: begin // OP_1
-            ;
+            rx_tready = 1'b1;
+            idiv = 1'b0;
+            if (rx_tvalid) begin
+                idiv = 1'b0;
+                frame_cnt_d = frame_cnt_q + 1;
+                operand_1_d = rx_data;
+                operand_2_d = 32'd1;
+                state_d = 4'd6;
+            end else begin
+                frame_cnt_d = frame_cnt_q;
+                operand_1_d = operand_1_q;
+                operand_2_d = operand_1_q;
+                state_d = 4'd5;
+            end
         end
 
+        4'd6: begin // OP_2
+            rx_tready = 1'b1;
+            idiv = 1'b0;
+            if (rx_tvalid) begin
+                frame_cnt_d = frame_cnt_q + 1;
+                operand_2_d = rx_data;
+                state_d = 4'd7;
+            end else begin
+                frame_cnt_d = frame_cnt_q;
+                operand_2_d = operand_2_q;
+                state_d = 4'd6;
+            end
+        end
+
+        4'd7: begin
+            if (cnt_q != 1'b1) begin
+                cnt_d = cnt_q + 1;
+            end else begin
+                cnt_d = cnt_q;
+                idiv = 1'b1;
+                rx_tready = tx_tready;
+                if (tx_tready && div_valid) begin
+                    frame_cnt_d = frame_cnt_q + 1;
+                    tx_tvalid = 1'b1;
+                    tx_data = div_o[7:0];
+                    if (frame_cnt_d >= length_q) begin
+                        state_d = 4'd0;
+                        frame_cnt_d = '0;
+                    end else begin
+                        state_d = 4'd7;
+                        frame_cnt_d = frame_cnt_q + 1;
+                    end
+                end
+            end
+        end
+
+        // 4'd5: begin // DELAY
+        //     rx_tready = 1'b1;
+        //     if (rx_tvalid) begin
+        //         frame_cnt_d = frame_cnt_q;
+        //         state_d = 4'd6;
+        //     end else begin
+        //         frame_cnt_d = frame_cnt_q;
+        //         state_d = 4'd5;
+        //     end
+        // end
+
+        // 4'd6: begin // OP_1
+        //     rx_tready = 1'b1;
+        //     idiv = 1'b0;
+        //     if (rx_tvalid) begin
+        //         idiv = 1'b0;
+        //         frame_cnt_d = frame_cnt_q + 1;
+        //         operand_1_d = rx_data;
+        //         operand_2_d = 32'd1;
+        //         state_d = 4'd7;
+        //     end else begin
+        //         frame_cnt_d = frame_cnt_q;
+        //         operand_1_d = operand_1_q;
+        //         operand_2_d = operand_1_q;
+        //         state_d = 4'd6;
+        //     end
+        // end
+
+        // 4'd7: begin // OP_2
+        //     rx_tready = 1'b1;
+        //     idiv = 1'b0;
+        //     if (rx_tvalid) begin
+        //         frame_cnt_d = frame_cnt_q + 1;
+        //         operand_2_d = rx_data;
+        //         state_d = 4'd8;
+        //     end else begin
+        //         frame_cnt_d = frame_cnt_q;
+        //         operand_2_d = operand_2_q;
+        //         state_d = 4'd7;
+        //     end
+        // end
+
+        // 4'd8: begin
+        //     if (cnt_q != 1'b1) begin
+        //         cnt_d = cnt_q + 1;
+        //     end else begin
+        //         cnt_d = cnt_q;
+        //         idiv = 1'b1;
+        //         rx_tready = tx_tready;
+        //         if (tx_tready && div_valid) begin
+        //             frame_cnt_d = frame_cnt_q + 1;
+        //             tx_tvalid = 1'b1;
+        //             tx_data = div_o[7:0];
+        //             if (frame_cnt_d >= length_q) begin
+        //                 state_d = 4'd0;
+        //                 frame_cnt_d = '0;
+        //             end else begin
+        //                 state_d = 4'd8;
+        //                 frame_cnt_d = frame_cnt_q + 1;
+        //             end
+        //         end
+        //     end
+        // end
     endcase
 end
 
